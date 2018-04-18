359ba5d80db0cad29dbdde6e5f7ff0a73f3ddcbc
Grygon <grygohn@gmail.com>
2018-04-17 20:50:02 -0400

    Add CSV reading
    Added function to read CSVs, reformatted CSV so it will work

--- sim.c
+++ sim.c
@@ -13,8 +13,9 @@
 // Create pointers to functions
 void forkSoln();
 void *threadSoln();
-struct planet updatePlanet(struct planet system[]);
+struct planet updatePlanet(struct planet planetSystem[]);
 struct vec vecAdd(struct vec v_1, struct vec v_2);
+void readCSV(struct planet planetSystem[]);
 
 // Vector struct
 struct vec {
@@ -28,7 +29,7 @@ struct vec {
 struct planet {
 	double mass; // Intrinsic property
 
-	struct vec x; // Position in vector
+	struct vec p; // Position in vector
 	struct vec v; // Velocity in vector
 	struct vec a; // Acceleration in vector
 }
@@ -45,11 +46,9 @@ int main (int argc, char *argv[]) {
 
 	// Add in timing monitoring TODO
 
-	// Read in starting data at given time TODO
-	struct planet startData[9];
+	// Read in starting data at given time
+	readCSV("startData.csv");
 
-
-	solarSystem = startData;
 
 	// Run one solution at a time to compare
 
@@ -92,10 +91,68 @@ struct vec delta(struct vec v_1, struct vec v_2) {
 }
 
 
-// Updates the properties of the "active" planet using the rest of the system
+// Adapted from http://c-for-dummies.com/blog/?p=2355
+// Reads the given file and places it in the solarSystem
+void readCSV(char filename[]) {
+	int BSIZE = 80;
+	char buffer[BSIZE];
+	FILE *f;
+	char *field;
+
+	/* open the CSV file */
+	f = fopen(filename,"r");
+	if( f == NULL)
+	{
+		printf("Unable to open file '%s'\n",filename);
+		exit(1);
+	}
+
+	// Index for read body
+	int i = 0;
+
+	/* process the data */
+	/* the file contains 8 fields */
+	while(fgets(buffer,BSIZE,f))
+	{
+		/* skip name */
+		field=strtok(buffer,",");
+		/* get x position */
+		field=strtok(NULL,",");
+		solarSystem[i]->p->x=atoi(field);
+		/* get y position */
+		field=strtok(NULL,",");
+		solarSystem[i]->p->y=atoi(field);
+		/* get z position */
+		field=strtok(NULL,",");
+		solarSystem[i]->p->z=atoi(field);
+		/* get x vel */
+		field=strtok(NULL,",");
+		solarSystem[i]->v->x=atoi(field);
+		/* get y vel */
+		field=strtok(NULL,",");
+		solarSystem[i]->v->y=atoi(field);
+		/* get z vel */
+		field=strtok(NULL,",");
+		solarSystem[i]->v->z=atoi(field);
+		/* get mass */
+		field=strtok(NULL,",");
+		solarSystem[i]->mass=atoi(field);
+
+		i++;
+	}
+
+	/* close file */
+	fclose(f);
+
+	return(0);
+}
+
+
+
+// Updates the properties of the "active" planet using the rest of the 1
 // System is passed in as an array of planets, the active planet is determined based on its index in that array
 // NOTE: Synchronization does not happen here. The system may occasionally be out-of-sync, but this function doesn't care
-struct planet updatePlanet(struct planet system[], int active) { 
+struct planet updatePlanet(struct planet [], int active) { 
 
 }
 
